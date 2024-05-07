37c13ca2d17eaa08d7935e08263a645e28976800
TryToLiveAlon_owner <156423343+TryToLiveAlone@users.noreply.github.com>
2024-05-07 20:29:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,8 +40,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VIP_CREATORS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TG_FRIENDSS")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/deathmovie_hub")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TryToLiveAlon")
 
 # Maximum Limit Allowed for users to save playlists on bot's server
 SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "100"))
@@ -117,18 +117,18 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+    "PING_IMG_URL", "https://mallucampaign.in/images/img_1713366408.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/ecdeac045f4045c198bd6.jpg"
+PLAYLIST_IMG_URL = "https://mallucampaign.in/images/img_1713366408.jpg"
+STATS_IMG_URL = "https://mallucampaign.in/images/img_1713366408.jpg"
+TELEGRAM_AUDIO_URL = "https://mallucampaign.in/images/img_1713366408.jpg"
+TELEGRAM_VIDEO_URL = "https://mallucampaign.in/images/img_1713366408.jpg"
+STREAM_IMG_URL = "https://mallucampaign.in/images/img_1713366408.jpg"
+SOUNCLOUD_IMG_URL = "https://mallucampaign.in/images/img_1713366408.jpg"
+YOUTUBE_IMG_URL = "https://mallucampaign.in/images/img_1713366408.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://mallucampaign.in/images/img_1713366408.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://mallucampaign.in/images/img_1713366408.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://mallucampaign.in/images/img_1713366408.jpg"
 
 
 def time_to_seconds(time):
